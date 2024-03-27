0ba349dcb905b6125b9a3d93679fe3493fa80c63
gagan11678 <gagandeepsingh11678@gmail.com>
2024-03-27 23:25:52 +0530

    Automated Birthday Wisher complete

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-##################### Extra Hard Starting Project ######################
+import pandas as pd
+import random
+import smtplib
+from datetime import datetime as dt
 
-# 1. Update the birthdays.csv
+today = (dt.today().month, dt.today().day)
 
-# 2. Check if today matches a birthday in the birthdays.csv
+data = pd.read_csv("birthdays.csv")
+birthday_dic = {(row['month'], row['day']): row for _, row in data.iterrows()}
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# 2. Check if today matches a birthday in the birthdays.csv. if true, pick a random letter from templates and replace
+# [NAME]
+if today in birthday_dic:
+    birthday_person = birthday_dic[today]
+    letters = ["letter_templates/letter_1.txt", "letter_templates/letter_2.txt", "letter_templates/letter_3.txt"]
+    letter_path = random.choice(letters)
+    with open(file=letter_path, mode='r') as letter:
+        file = letter.read()
+        modified_letter = file.replace("[NAME]", birthday_person['name'])
 
-# 4. Send the letter generated in step 3 to that person's email address.
+    # 4. Send the letter generated in step 3 to that person's email address.
+    email = "g11oct2000@gmail.com"
+    birthday_person_email = birthday_person['email']
+    password = "hwqk mvdh iejf jodu"
 
-
-
-
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(email, password)
+        connection.sendmail(from_addr=email, to_addrs=birthday_person_email, msg=f"subject: Happy birthday\n\n"
+                                                                                 "{modified_letter}")
